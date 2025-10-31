86af050f8a2679ec1c7c4af9968460ae94a4653e
Ahmed Ezz <ahmedezz9877@gmail.com>
2025-10-31 22:32:55 +0200

    Implement SignUpRepoContract interface and update SignUpUseCase to use it

--- lib/features/signup/domain/repositories/sign_up_repo_contract.dart
+++ lib/features/signup/domain/repositories/sign_up_repo_contract.dart
@@ -4,6 +4,6 @@ import '../../../../core/errors/failure.dart';
 import '../entities/sign_up_entity.dart';
 import '../entities/user_entity.dart';
 
-abstract interface class SignUpRepo {
+abstract interface class SignUpRepoContract {
   Future<Either<Failure, SignUpEntity>> signUp({required UserEntity user});
 }
--- lib/features/signup/domain/usecases/sign_up_use_case.dart
+++ lib/features/signup/domain/usecases/sign_up_use_case.dart
@@ -1,13 +1,15 @@
 import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
 
 import '../../../../core/errors/failure.dart';
 import '../entities/sign_up_entity.dart';
 import '../entities/user_entity.dart';
-import '../repositories/sign_up_repo.dart';
+import '../repositories/sign_up_repo_contract.dart';
 
+@injectable
 class SignUpUseCase {
   SignUpUseCase({required this.signUpRepo});
-  final SignUpRepo signUpRepo;
+  final SignUpRepoContract signUpRepo;
 
   Future<Either<Failure, SignUpEntity>> call({required UserEntity user}) {
     return signUpRepo.signUp(user: user);
