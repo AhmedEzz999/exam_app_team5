9cfb84fc29dde34549b8ed04a2ad8cbe1cb136d2
Ahmed Radwan <tech.ahmedradwan@gmail.com>
2025-11-01 03:59:19 +0200

    finish create a bottom nav bar with it's logic

--- lib/features/home_layout/presentation/views/widgets/custom_bottom_nav_bar.dart
+++ lib/features/home_layout/presentation/views/widgets/custom_bottom_nav_bar.dart
@@ -1,27 +1,11 @@
-import 'package:exam_app/core/constants/app_strings/app_strings.dart';
 import 'package:exam_app/core/styles/app_colors.dart';
-import 'package:exam_app/core/styles/assets.gen.dart';
-import 'package:exam_app/features/home_layout/helper/home_layout_enum.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import 'custom_bottom_nav_bar_item.dart';
 
-class CustomBottomNavBar extends StatefulWidget {
-  const CustomBottomNavBar({super.key});
-
-  @override
-  State<CustomBottomNavBar> createState() => _CustomBottomNavBarState();
-}
-
-class _CustomBottomNavBarState extends State<CustomBottomNavBar> {
-  late HomeLayoutEnum _homeLayoutEnum;
-  @override
-  void initState() {
-    _homeLayoutEnum = HomeLayoutEnum.explore;
-    super.initState();
-  }
-
+class CustomBottomNavBar extends StatelessWidget {
+  const CustomBottomNavBar({super.key, required this.navItems});
+  final List<Widget> navItems;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -30,44 +14,7 @@ class _CustomBottomNavBarState extends State<CustomBottomNavBar> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         spacing: 8.w,
-        children: [
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                _homeLayoutEnum = HomeLayoutEnum.explore;
-              });
-            },
-            child: CustomBottomNavBarItem(
-              icon: Assets.icons.homeIcon.svg(),
-              label: AppStrings.explore,
-              isSelected: _homeLayoutEnum == HomeLayoutEnum.explore,
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                _homeLayoutEnum = HomeLayoutEnum.result;
-              });
-            },
-            child: CustomBottomNavBarItem(
-              icon: Assets.icons.resultIcon.svg(),
-              label: AppStrings.result,
-              isSelected: _homeLayoutEnum == HomeLayoutEnum.result,
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                _homeLayoutEnum = HomeLayoutEnum.profile;
-              });
-            },
-            child: CustomBottomNavBarItem(
-              icon: Assets.icons.profileIcon.svg(),
-              label: AppStrings.profile,
-              isSelected: _homeLayoutEnum == HomeLayoutEnum.profile,
-            ),
-          ),
-        ],
+        children: navItems,
       ),
     );
   }
